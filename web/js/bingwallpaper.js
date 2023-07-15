6fdfa9276a362cb4838b18b24971492d94df146f
吃橡面丸子的玩乐行家 <steve5wutongyu6@163.com>
2023-07-15 22:28:37 +0800

    构建一个网页,通过js调取每天最新的图片

---
+++ web/js/bingwallpaper.js
@@ -0,0 +1,19 @@
+    // 替换为您的 OSS 地址
+    const ossBaseUrl = 'https://example.oss-cn-hongkong.aliyuncs.com/bingwallpaper/';
+
+    // 获取当前日期
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+
+    // 构建图片 URL
+    const imageUrl = ossBaseUrl + year + month + day + '.jpg';
+
+    // 创建图片元素
+    const imageElement = document.createElement('img');
+    imageElement.src = imageUrl;
+
+    // 添加图片到容器
+    const wallpaperContainer = document.getElementById('wallpaperContainer');
+    wallpaperContainer.appendChild(imageElement);
