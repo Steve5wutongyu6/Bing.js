6de4fbd2ea625a88ebec86ee51b789d232853c7e
Steve5wutongyu6 <77491648+Steve5wutongyu6@users.noreply.github.com>
2023-07-16 07:40:09 +0800

    bug修复,增加新功能
    1.修复了无法获取新壁纸的bug
    2.控制台输出执行时间日期
    3.自动删除本地前一天缓存的图片,但是OSS中的图片不会自动删除,建议自行前往OSS控制台设置定时删除策略

--- bing.js
+++ bing.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 const OSS = require('ali-oss');
+const moment = require('moment');
 
 // 阿里云 OSS 配置
 const ossConfig = {
@@ -12,13 +13,22 @@ const ossConfig = {
 };
 
 // Bing API 配置
-const bingApiUrl = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US';
+const bingApiUrl = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN';
 
 async function downloadBingWallpaper() {
   try {
-    const response = await axios.get(bingApiUrl);
+    const currentDate = moment().utcOffset('+08:00'); // 获取当前北京时间
+    const formattedDate = currentDate.format('YYYYMMDD'); // 格式化日期
+
+    // 删除前一天的壁纸
+    const previousDate = currentDate.subtract(1, 'day').format('YYYYMMDD');
+    const previousImageFileName = previousDate + '.jpg';
+    deleteLocalWallpaper(previousImageFileName);
+
+    const apiUrl = `${bingApiUrl}&date=${formattedDate}`;
+    const response = await axios.get(apiUrl);
     const imageUrl = `https://www.bing.com${response.data.images[0].url}`;
-    const imageFileName = response.data.images[0].startdate + '.jpg';
+    const imageFileName = formattedDate + '.jpg';
 
     const imageResponse = await axios.get(imageUrl, { responseType: 'stream' });
     const imagePath = `./${imageFileName}`;
@@ -30,6 +40,15 @@ async function downloadBingWallpaper() {
       writer.on('finish', () => resolve(imagePath));
       writer.on('error', reject);
     });
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+async function deleteLocalWallpaper(imageFileName) {
+  try {
+    fs.unlinkSync(`./${imageFileName}`);
+    console.log('Previous wallpaper deleted:', imageFileName);
   } catch (error) {
     console.error('Error:', error.message);
   }
@@ -52,12 +71,16 @@ async function uploadToOSS(imagePath) {
     console.error('Error:', error.message);
     console.error('Error occurred during upload:', error);
   }
-  
 }
 
-downloadBingWallpaper().then((imagePath) => {
-  console.log('Bing wallpaper saved:', imagePath);
-  uploadToOSS(imagePath);
-}).catch((error) => {
-  console.error('Error:', error.message);
-});
+const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
+console.log('Script started at:', currentTime);
+
+downloadBingWallpaper()
+  .then((imagePath) => {
+    console.log('Bing wallpaper saved:', imagePath);
+    uploadToOSS(imagePath);
+  })
+  .catch((error) => {
+    console.error('Error:', error.message);
+  });
