36198bfad86e157fc24ac0f9f6900c81f907d614
吃橡面丸子的玩乐行家 <steve5wutongyu6@163.com>
2023-07-16 07:59:03 +0800

    Revert "bug修复,增加新功能"
    This reverts commit 6de4fbd2ea625a88ebec86ee51b789d232853c7e.

--- bing.js
+++ bing.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
 const OSS = require('ali-oss');
-const moment = require('moment');
 
 // 阿里云 OSS 配置
 const ossConfig = {
@@ -13,22 +12,13 @@ const ossConfig = {
 };
 
 // Bing API 配置
-const bingApiUrl = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN';
+const bingApiUrl = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US';
 
 async function downloadBingWallpaper() {
   try {
-    const currentDate = moment().utcOffset('+08:00'); // 获取当前北京时间
-    const formattedDate = currentDate.format('YYYYMMDD'); // 格式化日期
-
-    // 删除前一天的壁纸
-    const previousDate = currentDate.subtract(1, 'day').format('YYYYMMDD');
-    const previousImageFileName = previousDate + '.jpg';
-    deleteLocalWallpaper(previousImageFileName);
-
-    const apiUrl = `${bingApiUrl}&date=${formattedDate}`;
-    const response = await axios.get(apiUrl);
+    const response = await axios.get(bingApiUrl);
     const imageUrl = `https://www.bing.com${response.data.images[0].url}`;
-    const imageFileName = formattedDate + '.jpg';
+    const imageFileName = response.data.images[0].startdate + '.jpg';
 
     const imageResponse = await axios.get(imageUrl, { responseType: 'stream' });
     const imagePath = `./${imageFileName}`;
@@ -40,15 +30,6 @@ async function downloadBingWallpaper() {
       writer.on('finish', () => resolve(imagePath));
       writer.on('error', reject);
     });
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-}
-
-async function deleteLocalWallpaper(imageFileName) {
-  try {
-    fs.unlinkSync(`./${imageFileName}`);
-    console.log('Previous wallpaper deleted:', imageFileName);
   } catch (error) {
     console.error('Error:', error.message);
   }
@@ -71,16 +52,12 @@ async function uploadToOSS(imagePath) {
     console.error('Error:', error.message);
     console.error('Error occurred during upload:', error);
   }
+  
 }
 
-const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
-console.log('Script started at:', currentTime);
-
-downloadBingWallpaper()
-  .then((imagePath) => {
-    console.log('Bing wallpaper saved:', imagePath);
-    uploadToOSS(imagePath);
-  })
-  .catch((error) => {
-    console.error('Error:', error.message);
-  });
+downloadBingWallpaper().then((imagePath) => {
+  console.log('Bing wallpaper saved:', imagePath);
+  uploadToOSS(imagePath);
+}).catch((error) => {
+  console.error('Error:', error.message);
+});
